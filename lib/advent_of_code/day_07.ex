c79fa83f3337dfbd8bbaa2c0f988a9d334e57d78
matthew.cobbing <cobbinma@gmail.com>
2023-12-07 19:03:27 +0000

    day 7 👑

---
+++ lib/advent_of_code/day_07.ex
@@ -0,0 +1,100 @@
+defmodule AdventOfCode.Day07 do
+  @rank_order ["A", "K", "Q", "J", "T", "9", "8", "7", "6", "5", "4", "3", "2"]
+  @rank_joker_order ["A", "K", "Q", "T", "9", "8", "7", "6", "5", "4", "3", "2", "J"]
+
+  def part1(input) do
+    input
+    |> String.split("\n", trim: true)
+    |> Enum.map(&String.split(&1, " ", trim: true))
+    |> Enum.map(fn [hand, stake] -> {String.graphemes(hand), String.to_integer(stake)} end)
+    |> Enum.sort(fn a, b -> compare_hands(a, b, &calculate_hand/1, @rank_order) end)
+    |> Enum.with_index()
+    |> Enum.reduce(0, fn {{_, stake}, index}, acc -> (index + 1) * stake + acc end)
+  end
+
+  def part2(input) do
+    input
+    |> String.split("\n", trim: true)
+    |> Enum.map(&String.split(&1, " ", trim: true))
+    |> Enum.map(fn [hand, stake] -> {String.graphemes(hand), String.to_integer(stake)} end)
+    |> Enum.sort(fn a, b -> compare_hands(a, b, &calculate_joker_hand/1, @rank_joker_order) end)
+    |> Enum.with_index()
+    |> Enum.reduce(0, fn {{_, stake}, index}, acc -> (index + 1) * stake + acc end)
+  end
+
+  def compare_hands({hand1, _}, {hand2, _}, calculate_hand, rank_order) do
+    score1 = calculate_hand.(hand1)
+    score2 = calculate_hand.(hand2)
+
+    if score1 == score2 do
+      Enum.zip(hand1, hand2)
+      |> Enum.reduce_while([], fn {a, b}, acc ->
+        a = Enum.find_index(rank_order, fn x -> x == a end)
+        b = Enum.find_index(rank_order, fn x -> x == b end)
+
+        if a == b do
+          {:cont, acc}
+        else
+          {:halt, a > b}
+        end
+      end)
+    else
+      score1 <= score2
+    end
+  end
+
+  def calculate_hand(hand) do
+    count =
+      Enum.reduce(hand, %{}, fn card, acc ->
+        Map.update(acc, card, 1, fn exist -> exist + 1 end)
+      end)
+
+    max = Map.values(count) |> Enum.max()
+    size = Kernel.map_size(count)
+
+    case {max, size} do
+      {5, _} -> 7
+      {4, _} -> 6
+      {3, size} when size == 2 -> 5
+      {3, _} -> 4
+      {2, size} when size == 3 -> 3
+      {2, _} -> 2
+      {1, _} -> 1
+    end
+  end
+
+  def calculate_joker_hand(hand) do
+    count =
+      Enum.reduce(hand, %{}, fn card, acc ->
+        Map.update(acc, card, 1, fn exist -> exist + 1 end)
+      end)
+      |> Kernel.then(fn count ->
+        jokers = Map.get(count, "J")
+
+        if jokers == 5 do
+          count
+        else
+          count =
+            Map.delete(count, "J")
+
+          {highest, _} =
+            Enum.max_by(count, fn {_, v} -> v end)
+
+          Map.update(count, highest, 5, fn existing -> existing + (jokers || 0) end)
+        end
+      end)
+
+    size = Kernel.map_size(count)
+    max = Map.values(count) |> Enum.max()
+
+    case {max, size} do
+      {5, _} -> 7
+      {4, _} -> 6
+      {3, size} when size == 2 -> 5
+      {3, _} -> 4
+      {2, size} when size == 3 -> 3
+      {2, _} -> 2
+      {1, _} -> 1
+    end
+  end
+end
---
+++ test/advent_of_code/day_07_test.exs
@@ -0,0 +1,33 @@
+defmodule AdventOfCode.Day07Test do
+  use ExUnit.Case
+
+  import AdventOfCode.Day07
+
+  test "part1" do
+    input = """
+    32T3K 765
+    T55J5 684
+    KK677 28
+    KTJJT 220
+    QQQJA 483 
+    """
+
+    result = part1(input)
+
+    assert result == 6440
+  end
+
+  test "part2" do
+    input = """
+    32T3K 765
+    T55J5 684
+    KK677 28
+    KTJJT 220
+    QQQJA 483
+    """
+
+    result = part2(input)
+
+    assert result == 5905
+  end
+end
